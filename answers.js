912d158b9905f73cd6edd162bedfa95401f8ba33
ljmcdade <ljmcdade@gmail.com>
2015-11-13 19:14:55 +0000

    exercise one first letter of name

--- answers.js
+++ answers.js
@@ -1,1 +1,21 @@
 /* this is a one line comment */
+/* this function is to return and print the letter of a name: */
+function firstLetter(string) { 
+    console.log(string.charAt(0));
+    
+};
+firstLetter("Lauren");
+ 
+ /* or I could use: */
+ 
+console.log(firstLetter("Lauren"));
+
+/* testing it out: */
+
+console.log(firstLetter("Peter"));
+console.log(firstLetter(" "));
+
+
+
+
+
